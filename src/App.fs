797b6d373bcc1bd8fb69b2139fe03761cd7899d1
KATZUG <web-admin@kat-zug-rettungshundestaffel.at>
2022-12-25 19:19:09 +0100

    added tokenInfo

--- src/App.fs
+++ src/App.fs
@@ -3,24 +3,56 @@ module App
 open Elmish
 open Elmish.React
 open Feliz
+open Fetch
+open Thoth.Fetch
 
 type State =
-    { Count: int }
+    { Count: int
+      Message: string option
+      Fetching: bool }
 
 type Msg =
     | Increment
-    | Decrement
+    | FetchingMessage
+    | MessageReceived of string
+    | MessageError of exn
+    
 
 let init() =
-    { Count = 0 }
+    { Count = 0
+      Message = None
+      Fetching = false },
+    Cmd.none
 
-let update (msg: Msg) (state: State): State =
+
+let update msg model =
     match msg with
-    | Increment ->
-        { state with Count = state.Count + 1 }
+    | Increment -> { model with Count = model.Count + 1 }, Cmd.none
+    | FetchingMessage ->
+        let getMessage () =
+            promise {
+                let! message =
+                    Fetch.get<unit, string> (
+                        "/.auth/me",
+                        headers = [ HttpRequestHeaders.Accept "application/json" ]
+                    )
 
-    | Decrement ->
-        { state with Count = state.Count - 1 }
+                return message
+            }
+
+        { model with Fetching = true }, Cmd.OfPromise.either getMessage () MessageReceived MessageError
+    | MessageReceived message ->
+        { model with
+              Message = Some message
+              Fetching = false },
+        Cmd.none
+    | MessageError err ->
+        printfn "%O" err
+
+        { model with
+              Fetching = false
+              Message = None },
+        Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
   Html.div [
@@ -28,15 +60,22 @@ let render (state: State) (dispatch: Msg -> unit) =
       prop.onClick (fun _ -> dispatch Increment)
       prop.text "Increment"
     ]
-
-    Html.button [
-      prop.onClick (fun _ -> dispatch Decrement)
-      prop.text "Decrement"
+    Html.button [ 
+      prop.onClick(fun _ -> dispatch FetchingMessage)
+      prop.text "Get a message from the server"
     ]
 
+
+    match (state.Fetching, state.Message) with
+    | (false, None) -> ()
+    | (false, Some msg) -> Html.p msg
+    | (true, _) -> Html.p "Fetching from server..."
+
+
     Html.h1 state.Count
+    
   ]
 
-Program.mkSimple init update render
+Program.mkProgram init update render
 |> Program.withReactSynchronous "elmish-app"
 |> Program.run
