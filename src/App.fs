5577d0b4c19cb732fb79f36dbd7a596b4e629969
plegerer <56559282+plegerer@users.noreply.github.com>
2022-12-27 11:13:21 +0100

    Update App.fs

--- src/App.fs
+++ src/App.fs
@@ -81,7 +81,7 @@ let update msg model =
             promise {
                 let! message =
                     Fetch.get(
-                        "https://plegerer-adauthenticationbackend-test.azurewebsites.net/.auth/me",
+                        "/.auth/me",
                         headers = [ HttpRequestHeaders.Accept "application/json" ],
                         decoder = ClaimResult.decoder
                     )
